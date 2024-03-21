18843c619d2c0a0b65aadfa37dfad6b680ad9d6f
Nursantia22 <147220016+Nursantia22@users.noreply.github.com>
2024-03-20 22:46:19 -0700

    Add files via upload

---
+++ inputan.py
@@ -0,0 +1,9 @@
+# Input nama dan mata kuliah
+nama_kalian = input("Masukkan nama Anda: ")
+mata_kuliah = input("Masukkan mata kuliah yang sedang Anda pelajari: ")
+
+# Membuat kalimat selamat datang
+kalimat_selamat_datang = f"Hallo Selamat Datang {nama_kalian}, selamat belajar {mata_kuliah}!"
+
+# Menampilkan output
+print(kalimat_selamat_datang)
